7f6c3737864f2c0c6e9ae4f39d9360773cf2e866
bjaram <bjaram@ucenfotec.ac.cr>
2025-06-11 20:25:00 -0600

    initial commit

---
+++ EjemplosPatrones/Creacionales/SingletonPattern/ClienteEjemplo.cs
@@ -0,0 +1,19 @@
+﻿namespace EjemplosPatrones.Creacionales.SingletonPatron;
+
+public class ClienteEjemplo
+{
+    public void EjecutarEjemplo()
+    {
+        var singleton = Singleton.GetInstance();
+        singleton.Nombre = "Bryan";
+        singleton.Edad = 21;
+        
+        Console.WriteLine(singleton.Nombre);
+        Console.WriteLine(singleton.Edad);
+        
+        var singleton2 = Singleton.GetInstance();
+        singleton2.Nombre = "Luis";
+        
+        Console.WriteLine(singleton.Nombre); //Luis
+    }
+}
---
+++ EjemplosPatrones/Program.cs
@@ -0,0 +1,12 @@
+﻿using EjemplosPatrones.Creacionales.SingletonPatron;
+
+namespace EjemplosPatrones;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var ejemplo = new ClienteEjemplo();
+        ejemplo.EjecutarEjemplo();
+    }
+}
